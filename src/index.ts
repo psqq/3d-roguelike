98486d2c6a19c2351bbe5c5358b9361299c1eecc
Sergey Pestov <pestov-sa@yandex.ru>
2020-03-06 22:44:36 +0300

    start in 2d

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import './index.css';
+
+const can = document.querySelector('canvas');
+const ctx = can.getContext('2d');
+
+ctx.fillRect(50, 50, 100, 100);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  entry: './src/main.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  entry: './src/index.ts',
+  resolve: {
+    extensions: ['.ts', '.js']
   },
-};
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index.min.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+}
